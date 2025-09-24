ec0b4ab278f902f4e81919bbec4c340725330893
Joshua Dirga <joshdirga@gmail.com>
2025-09-24 18:26:46 -0400

    Fixing crashes

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"github.com/gin-gonic/gin"
 
   	"github.com/cuappdev/hustle-backend/models"
@@ -8,13 +9,15 @@ import (
 )
 
 func main() {
+	log.Println("Starting hustle-backend...")
   	r := gin.Default()
-
+	log.Println("Connecting to database...")
 	models.ConnectDatabase()
+	log.Println("Setting up routes...")
 	r.GET("/healthcheck", controllers.HealthCheck)
 	r.GET("/users", controllers.FindUsers)
 	r.POST("/users", controllers.CreateUser)
-
+    log.Println("Server starting on :8080")
   	r.Run()
 }
 
