f9a4bfe5f04a59b0a88993e41c892d8457b5327b
Joshua Dirga <joshdirga@gmail.com>
2025-10-01 17:38:41 -0400

    Fixed logging and load env

--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"log"
 	"github.com/gin-gonic/gin"
-
+	"github.com/joho/godotenv"
   	"github.com/cuappdev/hustle-backend/models"
   	"github.com/cuappdev/hustle-backend/controllers"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file.")
+	}
+
 	log.Println("Starting hustle-backend...")
   	r := gin.Default()
 	log.Println("Connecting to database...")
--- models/setup.go
+++ models/setup.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+    "fmt"
     "log"
     "os"
     "time"
@@ -10,7 +11,7 @@ import (
 
 var DB *gorm.DB
 
-func ConnectDatabase() {
+func ConnectDatabase() error {
     // Build connection string from env variables
     host := getEnv("DB_HOST", "localhost")
     user := getEnv("DB_USER", "postgres")
@@ -19,31 +20,32 @@ func ConnectDatabase() {
     password := getEnv("DB_PASSWORD", "")
     sslmode := getEnv("DB_SSLMODE", "require")
     
-    dsn := "host=" + host + " user=" + user + " password=" + password + " dbname=" + dbname + " port=" + port + " sslmode=" + sslmode + " TimeZone=UTC"
-    
+    dsn := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=%s", user, password, host, port, dbname, sslmode)
+    log.Println("Connecting to database with DSN:", dsn)
+
     database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
     if err != nil {
-        return err
+        return fmt.Errorf("failed to connect to database: %w", err)
     }
 
     sqlDB, err := database.DB()
     if err != nil {
-        return err
+        return fmt.Errorf("failed to get database instance: %w", err)
     }
     
     sqlDB.SetMaxIdleConns(10)
     sqlDB.SetMaxOpenConns(100)
     sqlDB.SetConnMaxLifetime(time.Hour)
 
-	// Make sure to include all models to migrate here
+    // Make sure to include all models to migrate here
     err = database.AutoMigrate(&User{})
     if err != nil {
-        log.Printf("Failed to migrate database: %v", err)
-        return err
+        return fmt.Errorf("failed to migrate database: %w", err)
     }
 
     DB = database
     log.Println("Database connected successfully")
+    return nil
 }
 
 func getEnv(key, defaultValue string) string {
